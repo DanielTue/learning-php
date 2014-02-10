8e90c2ae43f7e8be8ca59c8294fd021dad19c043
DanielTue <tuetravel@gmail.com>
2014-02-10 23:51:16 +0100

    Create calculator.php

---
+++ calculator.php
@@ -0,0 +1,20 @@
+<?php
+
+// String Calculator
+class StringCalculator
+{
+    public function add($string)
+    {
+        $sum = 0;
+        if (strlen($string) != 0 ) {
+            $numbers = preg_split("/[\\n|,]+/", $string);
+            foreach ($numbers as $number) {
+                $sum += $number;
+            }
+
+            return $sum;
+        } else {
+            return 0;
+        }
+    }
+}
